483876570180ce1a81162fb19e6a78e3a8fdc18c
hohyuncheon <cheon@blockchainglobal.co.kr>
2022-09-28 14:16:14 +0900

    22.09.28 거품정렬

---
+++ src/main/kotlin/00/정렬/거품정렬.kt
@@ -0,0 +1,20 @@
+import java.util.*
+
+class 거품정렬 {
+}
+
+fun main(args: Array<String>) {
+    val arr = intArrayOf(1, 5, 8, 9, 2)
+
+    var temp = 0
+    for (i in arr.indices) {
+        for (j in 1 until arr.size - i) {
+            if (arr[j - 1] > arr[j]) {
+                temp = arr[j - 1]
+                arr[j - 1] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    println(Arrays.toString(arr))
+}
