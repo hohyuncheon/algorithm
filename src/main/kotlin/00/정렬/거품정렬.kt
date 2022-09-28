e5607f485e59803c7dbdf198bc57aa13172781e0
천호현 <lila230011@gmail.com>
2022-09-28 17:00:07 +0900

    22.09.28 거품정렬

--- src/main/kotlin/00/정렬/거품정렬.kt
+++ src/main/kotlin/00/정렬/거품정렬.kt
@@ -1,5 +1,19 @@
 import java.util.*
 
+/*
+ Bubble Sort
+ 1회전이 끝난 후, 배열의 마지막 위치에는 가장 큰 원소가 위치하며 정렬된다.
+
+ 장점
+    구현이 매우 간단하고, 소스코드가 직관적이다.
+    정렬하고자 하는 배열 안에서 교환하는 방식이므로, 다른 메모리 공간을 필요로 하지 않다. => 제자리 정렬(in-place sorting)
+    안정 정렬(Stable Sort) 이다.
+
+ 단점
+    시간복잡도가 최악, 최선, 평균 모두 O(n^2)으로, 굉장히 비효율적이다.
+    정렬 돼있지 않은 원소가 정렬 됐을때의 자리로 가기 위해서, 교환 연산(swap)이 많이 일어나게 된다.
+ */
+
 class 거품정렬 {
 }
 
@@ -17,6 +31,4 @@ fun main(args: Array<String>) {
         }
     }
     println(Arrays.toString(arr))
-    println("test")
-    println("test")
 }
